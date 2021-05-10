ff1cbd8a5d30bad229d0e560adae1542974eadf4
JoinGithub500 <66752532+Yerer500@users.noreply.github.com>
2021-05-10 23:09:32 +0300

    giving a initialized value
    I give a initialized value of  the structure of Recipe, recX and Menu menuX

--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@ struct Recipe{
     int numOfComponents;
     string c_List;
     string c_Measurement;  
-};
+}recX{
+	  0,
+	  {"Salt","Oil","Bread","Injer","Onion"},
+	  {"gram","mililiter"}
+    };
+    
 struct Menu{
     int itemCode;
     float itemPrice;
     int preparationTime;
     Recipe itemRecipe;
-};
+}menuX{
+	   {1,2,3,4,5,6,7,8,9,10},
+	   {23.4,45.5,76,45,73,99,69.6,50.0,125.9,30.00},
+	   {25,45,35,50,30,65,35,35,45,65}
+    };
+    
 struct Order{
     string orderedBy;
     Menu menuItems[5];
@@ -26,7 +36,6 @@ struct Order{
 
 
 int main(){
-	cout<<"Enter a name";
 	
 	return 0;
 }
