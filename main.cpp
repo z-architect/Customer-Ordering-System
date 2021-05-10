51136dd332f3eb2df799e9dd9ff52b41c528bbc8
JoinGithub500 <66752532+Yerer500@users.noreply.github.com>
2021-05-10 22:54:30 +0300

    created a struct of three
    I have created structure of three, and the are nested with each other.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+// created a sturcture of three and nested them with eachother
+struct Recipe{
+    int numOfComponents;
+    string c_List;
+    string c_Measurement;  
+};
+struct Menu{
+    int itemCode;
+    float itemPrice;
+    int preparationTime;
+    Recipe itemRecipe;
+};
+struct Order{
+    string orderedBy;
+    Menu menuItems[5];
+};
+
+
+
+
+
+
+
 int main(){
-	cout<<"I am working";
+	cout<<"Enter a name";
 	
 	return 0;
 }
