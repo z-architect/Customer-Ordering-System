fd0e49da7605d46d8b37cc4b3d15a401a6d80ce5
JoinGithub500 <66752532+Yerer500@users.noreply.github.com>
2021-05-12 22:02:31 +0300

    created a function initialize and showMenuList
    Two function 1.showMenuList  to show us the order for the user and 2.initialize the value of the first item which is Beyaynet

--- main.cpp
+++ main.cpp
@@ -1,41 +1,84 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-
-// created a sturcture of three and nested them with each other
 struct Recipe{
-    int numOfComponents;
-    string c_List;
-    string c_Measurement;  
-}recX{
-	  0,
-	  {"Salt","Oil","Bread","Injer","Onion"},
-	  {"gram","mililiter"}
-    };
-    
+	    int numOfComponents;
+	    string c_List[20];
+	    string c_Measurement;  
+};
+	    
 struct Menu{
-    int itemCode;
-    float itemPrice;
-    int preparationTime;
-    Recipe itemRecipe;
-}menuX{
-	   {1,2,3,4,5,6,7,8,9,10},
-	   {23.4,45.5,76,45,73,99,69.6,50.0,125.9,30.00},
-	   {25,45,35,50,30,65,35,35,45,65}
-    };
-    
+	    string menuName;
+	    int itemCode;
+	    float itemPrice;
+	    int preparationTime;
+	    Recipe itemRecipe;
+}menuX[10];
+		    
 struct Order{
-    string orderedBy;
-    Menu menuItems[5];
+	    string orderedBy;
+	    Menu menuItems[5];
 };
 
+// prototyping a function
+void initialize();
+void showMenuList();
 
 
-
-
-
+// a global variable
+int numOfMenuItems;
 
 int main(){
-	
+	showMenuList();
+
 	return 0;
 }
+
+
+void showMenuList (){
+	ofstream menuList("menuTable.txt");
+	menuList<<"    ****************************** MENU LIST ***********************************"<<endl;
+ 	menuList<<" "<<endl;
+    menuList<<"                Daily-food                         Fast-Food    "<<endl;                        
+    menuList<<"                **********                         *********    "<<endl;                       
+    menuList<<"              01. Beyaynet                       06. Burger     "<<endl;                     
+    menuList<<"              02. Shiro                          07. Pizza      "<<endl;                 
+    menuList<<"              03. Kitfo                          08. Chicken    "<<endl;                      
+    menuList<<"              04. Doro-wat                       09. Sandwich   "<<endl;                         
+    menuList<<"              05. Kita-fir-fir                   10. Tacos      "<<endl;                       
+	menuList.close();
+	
+	string itemLine;
+	ifstream readFile("menuTable.txt");
+	while(getline(readFile, itemLine)){
+		cout<<itemLine<<endl;
+	}
+	readFile.close();
+	
+}
+
+
+
+void initialize(){
+	// Initializing the first menu Item
+	menuX[0].menuName = "Beyaynet";
+	menuX[0].itemCode = 1;
+	menuX[0].itemPrice = 35.5;
+	menuX[0].preparationTime = 30;
+	menuX[0].itemRecipe.numOfComponents = 3;
+	menuX[0].itemRecipe.c_List[0] = " ";
+	menuX[0].itemRecipe.c_List[1] = " ";
+	menuX[0].itemRecipe.c_List[2] = " ";
+	menuX[0].itemRecipe.c_Measurement = "Gram";
+}
+
+
+
+
+
+
+
+
+
+
